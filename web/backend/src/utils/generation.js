e441fbf123971e76c48b88e701d8395d25d56da3
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-12-09 16:50:55 +0100

    Add generation token and code

---
+++ web/backend/src/utils/generation.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+export const generateToken = (userId) => {
+    return jwt.sign({id: userId}, process.env.JWT_SECRET_KEY, {
+        expiresIn: "7d",
+    });
+};
+
+export const generateVerificationToken = () => {
+    return crypto.randomBytes(32).toString("hex");
+};
+
+export const generateVerificationCode = () => {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+}
