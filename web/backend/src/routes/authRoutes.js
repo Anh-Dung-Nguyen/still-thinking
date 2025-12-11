6f0ca0df82e5887333ab42415d59c31a3cd3d2e6
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-12-11 19:54:03 +0100

    Add some endpoints for signing in and signing out

--- web/backend/src/routes/authRoutes.js
+++ web/backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { checkAvailability, resendVerificationCode, resendVerificationEmail, resendVerificationPhone, signup, verifyCode, verifyEmail, verifyPhone } from "../controllers/authControllers.js";
+import { checkAvailability, forgotPassword, getMe, refreshToken, resendVerificationCode, resendVerificationEmail, resendVerificationPhone, resetPassword, signin, signout, signup, verifyCode, verifyEmail, verifyPhone, verifyResetCode } from "../controllers/authControllers.js";
+import { protect } from "../middleware/middleware.js";
 
 const router = express.Router();
 
@@ -11,5 +12,14 @@ router.post("/verify-phone", verifyPhone);
 router.post("/resend-code", resendVerificationCode);
 router.post("/resend-verification-email", resendVerificationEmail);
 router.post("/resend-verification-phone", resendVerificationPhone);
+
+router.post("/signin", signin);
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-reset-code", verifyResetCode);
+router.post("/reset-password", resetPassword);
+
+router.get("/me", protect, getMe);
+router.post("/signout", protect, signout);
+router.post("/refresh-token", protect, refreshToken);
 
 export default router;
